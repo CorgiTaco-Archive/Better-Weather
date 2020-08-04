c09bbaeb089d113e70ac2b582b655cd3375ae95c
CorgiTaco <>
2020-08-04 00:03:14 -0700

    Basic Events

--- src/main/java/corgitaco/betterweather/BetterWeather.java
+++ src/main/java/corgitaco/betterweather/BetterWeather.java
@@ -1,6 +1,9 @@
 package corgitaco.betterweather;
 
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -9,14 +12,37 @@ import org.apache.logging.log4j.Logger;
 @Mod("betterweather")
 public class BetterWeather {
     public static Logger LOGGER = LogManager.getLogger();
-
+    public static final String MOD_ID = "betterweather";
 
     public BetterWeather() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     }
 
     public void commonSetup(FMLCommonSetupEvent event) {
 
+    }
+
+    public void clientSetup(FMLClientSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = BetterWeather.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class BetterWeatherEvents {
+
+        @SubscribeEvent
+        public static void worldTick(TickEvent.WorldTickEvent event) {
+
+        }
+
+        @SubscribeEvent
+        public static void renderTickEvent(TickEvent.RenderTickEvent event) {
+
+        }
+
+        @SubscribeEvent
+        public static void playerTickEvent(TickEvent.PlayerTickEvent event) {
+
+        }
     }
 }
