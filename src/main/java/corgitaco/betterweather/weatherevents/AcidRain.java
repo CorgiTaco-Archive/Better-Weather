4c442270726630b26b43ee76e4d45f7d0676c6e8
CorgiTaco <>
2020-08-06 19:50:30 -0700

    Beta release.

--- src/main/java/corgitaco/betterweather/weatherevents/AcidRain.java
+++ src/main/java/corgitaco/betterweather/weatherevents/AcidRain.java
@@ -32,19 +32,20 @@ import java.util.Random;
 public class AcidRain {
     public static void acidRainEvent(Chunk chunk, ServerWorld world, int gameRuleTickSpeed, long worldTime) {
         ChunkPos chunkpos = chunk.getPos();
-        boolean flag = world.isRaining();
         int chunkXStart = chunkpos.getXStart();
         int chunkZStart = chunkpos.getZStart();
         IProfiler iprofiler = world.getProfiler();
         iprofiler.startSection("acidrain");
         BlockPos blockpos = world.getHeight(Heightmap.Type.MOTION_BLOCKING, world.getBlockRandomPos(chunkXStart, 0, chunkZStart, 15));
         if (world.isAreaLoaded(blockpos, 1)) {
-            if (BetterWeather.BetterWeatherEvents.weatherData.isAcidRain() && world.getWorldInfo().isRaining() && worldTime % BetterWeatherConfig.tickBlockDestroySpeed.get() == 0) {
+            if (BetterWeather.BetterWeatherEvents.weatherData.isAcidRain() && world.getWorldInfo().isRaining() && worldTime % BetterWeatherConfig.tickBlockDestroySpeed.get() == 0 && BetterWeatherConfig.destroyBlocks.get()) {
                 if (world.getBlockState(blockpos.down()).getBlock() == Blocks.GRASS_BLOCK)
                     world.setBlockState(blockpos.down(), Blocks.COARSE_DIRT.getDefaultState());
                 if (world.getBlockState(blockpos).getMaterial() == Material.PLANTS || world.getBlockState(blockpos).getMaterial() == Material.TALL_PLANTS)
                     world.setBlockState(blockpos, Blocks.AIR.getDefaultState());
                 if (world.getBlockState(blockpos.down()).getBlock().isIn(BlockTags.LEAVES))
+                    world.setBlockState(blockpos.down(), Blocks.AIR.getDefaultState());
+                if (world.getBlockState(blockpos.down()).getBlock().isIn(BlockTags.CROPS))
                     world.setBlockState(blockpos.down(), Blocks.AIR.getDefaultState());
             }
         }
