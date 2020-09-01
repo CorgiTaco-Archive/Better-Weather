0c316c2318de6beefc8d4097f58c44ced74ebd96
CorgiTaco <>
2020-09-01 15:43:05 -0700

    Aheeeheee

--- src/main/java/corgitaco/betterweather/client/BWClient.java
+++ src/main/java/corgitaco/betterweather/client/BWClient.java
@@ -5,11 +5,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class BWClient implements ClientModInitializer {
-
-
     @Override
     public void onInitializeClient() {
         ClientTickEvents.START_CLIENT_TICK.register(event -> BetterWeather.BetterWeatherEvents.clientTickEvent());
-
     }
 }
