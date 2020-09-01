3d9c758e14ff8d305d2a5753723573581a55e96d
CorgiTaco <>
2020-09-01 12:49:32 -0700

    Entity Ticking

---
+++ src/main/java/corgitaco/betterweather/mixin/common/entity/MixinLivingEntity.java
@@ -0,0 +1,18 @@
+package corgitaco.betterweather.mixin.common.entity;
+
+import corgitaco.betterweather.BetterWeather;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public class MixinLivingEntity {
+
+    @Inject(at = @At("HEAD"), method = "tick()V", cancellable = true)
+    private void getTick(CallbackInfo ci) {
+        BetterWeather.BetterWeatherEvents.entityTickEvent((LivingEntity)(Object)this);
+    }
+
+}
