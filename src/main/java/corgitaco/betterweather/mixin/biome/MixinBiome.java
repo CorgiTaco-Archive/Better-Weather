41bab68d5d18fb7c9c3b92d2ed6d8ed843641000
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-03-17 22:40:58 -0700

    -Successfully store and access the per world biome registry

--- src/main/java/corgitaco/betterweather/mixin/biome/MixinBiome.java
+++ src/main/java/corgitaco/betterweather/mixin/biome/MixinBiome.java
@@ -1,23 +1,15 @@
 package corgitaco.betterweather.mixin.biome;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import corgitaco.betterweather.helpers.IBiomeModifier;
-import net.minecraft.world.biome.*;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 
 @Mixin(Biome.class)
@@ -26,11 +18,6 @@ public class MixinBiome implements IBiomeModifier {
     @Shadow @Final private Biome.Climate climate;
     private Supplier<Float> tempModifier = null;
     private Supplier<Float> humidityModifier = null;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void injectFreezeTopLayer(Biome.Climate climate, Biome.Category category, float depth, float scale, BiomeAmbience effects, BiomeGenerationSettings biomeGenerationSettings, MobSpawnInfo mobSpawnInfo, CallbackInfo ci) {
-        addFeatureToBiome((Biome) (Object)this, GenerationStage.Decoration.TOP_LAYER_MODIFICATION, Features.FREEZE_TOP_LAYER);
-    }
 
     @Inject(method = "getDownfall", at = @At("RETURN"), cancellable = true)
     private void modifyDownfall(CallbackInfoReturnable<Float> cir) {
@@ -50,21 +37,5 @@ public class MixinBiome implements IBiomeModifier {
     @Override
     public void setHumidityModifier(float newValue) {
         this.humidityModifier = () -> newValue;
-    }
-
-    //Use these to add our features to vanilla's biomes.
-    private static void addFeatureToBiome(Biome biome, GenerationStage.Decoration feature, ConfiguredFeature<?, ?> configuredFeature) {
-        ConvertImmutableFeatures(biome);
-        List<List<Supplier<ConfiguredFeature<?, ?>>>> biomeFeatures = biome.getGenerationSettings().features;
-        while (biomeFeatures.size() <= feature.ordinal()) {
-            biomeFeatures.add(Lists.newArrayList());
-        }
-        biomeFeatures.get(feature.ordinal()).add(() -> configuredFeature);
-    }
-
-    private static void ConvertImmutableFeatures(Biome biome) {
-        if (biome.getGenerationSettings().features instanceof ImmutableList) {
-            biome.getGenerationSettings().features = biome.getGenerationSettings().features.stream().map(Lists::newArrayList).collect(Collectors.toList());
-        }
     }
 }
--- src/main/java/corgitaco/betterweather/mixin/server/MixinServerWorld.java
+++ src/main/java/corgitaco/betterweather/mixin/server/MixinServerWorld.java
@@ -1,20 +1,21 @@
 package corgitaco.betterweather.mixin.server;
 
 import corgitaco.betterweather.BetterWeatherUtil;
-import corgitaco.betterweather.api.SeasonData;
 import corgitaco.betterweather.api.weatherevent.WeatherData;
 import corgitaco.betterweather.datastorage.BetterWeatherEventData;
 import corgitaco.betterweather.helper.IsWeatherForced;
 import corgitaco.betterweather.helpers.IBiomeModifier;
 import corgitaco.betterweather.helpers.IBiomeUpdate;
-import corgitaco.betterweather.season.Season;
 import corgitaco.betterweather.util.WorldDynamicRegistry;
 import corgitaco.betterweather.weatherevent.weatherevents.WeatherEventUtil;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.NBTDynamicOps;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.registry.DynamicRegistries;
-import net.minecraft.util.registry.MutableRegistry;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.WorldSettingsImport;
+import net.minecraft.world.Dimension;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
@@ -22,10 +23,13 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.listener.IChunkStatusListener;
 import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.server.ServerChunkProvider;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.spawner.ISpecialSpawner;
+import net.minecraft.world.storage.IServerConfiguration;
 import net.minecraft.world.storage.IServerWorldInfo;
 import net.minecraft.world.storage.SaveFormat;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -48,6 +52,14 @@ public abstract class MixinServerWorld implements IBiomeUpdate {
     @Inject(method = "<init>", at = @At("RETURN"))
     private void storeUpgradablePerWorldRegistry(MinecraftServer server, Executor executor, SaveFormat.LevelSave save, IServerWorldInfo worldInfo, RegistryKey<World> key, DimensionType dimensionType, IChunkStatusListener statusListener, ChunkGenerator generator, boolean b, long seed, List<ISpecialSpawner> specialSpawners, boolean b1, CallbackInfo ci) {
         this.registry = new WorldDynamicRegistry((DynamicRegistries.Impl) server.func_244267_aX());
+
+        WorldSettingsImport<INBT> worldSettingsImport = WorldSettingsImport.create(NBTDynamicOps.INSTANCE, server.getDataPackRegistries().getResourceManager(), (DynamicRegistries.Impl) this.registry);
+        IServerConfiguration newServerConfiguration = save.readServerConfiguration(worldSettingsImport, server.getServerConfiguration().getDatapackCodec());
+        Dimension dimension = newServerConfiguration.getDimensionGeneratorSettings().func_236224_e_().getOptional(key.getLocation()).get();
+        ChunkGenerator dimensionChunkGenerator = dimension.getChunkGenerator();
+        this.field_241102_C_/*Server Chunk Provider*/.getChunkGenerator().field_235949_c_ = dimensionChunkGenerator.getBiomeProvider();
+        this.field_241102_C_/*Server Chunk Provider*/.getChunkGenerator().biomeProvider = dimensionChunkGenerator.getBiomeProvider();
+
         updateBiomeData();
     }
 
@@ -55,7 +67,6 @@ public abstract class MixinServerWorld implements IBiomeUpdate {
     public void updateBiomeData() {
         for (Map.Entry<RegistryKey<Biome>, Biome> entry : registry.getRegistry(Registry.BIOME_KEY).getEntries()) {
             Biome biome = entry.getValue();
-
             ((IBiomeModifier) (Object) biome).setHumidityModifier((float) uniqueTemp());
             ((IBiomeModifier) (Object) biome).setTempModifier((float) uniqueTemp());
         }
@@ -78,6 +89,10 @@ public abstract class MixinServerWorld implements IBiomeUpdate {
 
     @Shadow
     public IServerWorldInfo field_241103_E_;
+
+    @Shadow
+    @Final
+    private ServerChunkProvider field_241102_C_;
 
     @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$RuleKey;)Z", ordinal = 0))
     private boolean rollBetterWeatherEvent(GameRules gameRules, GameRules.RuleKey<GameRules.BooleanValue> key) {
