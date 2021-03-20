cd1589c14bf42663bffcde71c927c55e45e47fba
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-03-20 01:57:26 -0700

    -Add Season to Debug overlay

---
+++ src/main/java/corgitaco/betterweather/mixin/client/MixinDebugOverGui.java
@@ -0,0 +1,34 @@
+package corgitaco.betterweather.mixin.client;
+
+import corgitaco.betterweather.api.BetterWeatherWorldData;
+import corgitaco.betterweather.season.SeasonContext;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.overlay.DebugOverlayGui;
+import org.apache.commons.lang3.text.WordUtils;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(DebugOverlayGui.class)
+public class MixinDebugOverGui {
+
+
+    @Shadow
+    @Final
+    private Minecraft mc;
+
+    @Inject(method = "getDebugInfoLeft", at = @At("RETURN"))
+    private void addSeasonDebugText(CallbackInfoReturnable<List<String>> cir) {
+        if (this.mc.world != null) {
+            SeasonContext seasonContext = ((BetterWeatherWorldData) this.mc.world).getSeasonContext();
+            if (this.mc.gameSettings.showDebugInfo && seasonContext != null) {
+                cir.getReturnValue().add("Season: " + WordUtils.capitalize(seasonContext.getCurrentSeason().getSeasonKey().toString().toLowerCase()) + " | " + WordUtils.capitalize(seasonContext.getCurrentSeason().getCurrentPhase().toString().replace("_", "").toLowerCase()));
+            }
+        }
+    }
+}
