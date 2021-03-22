7c36f98ef2863bb78a90c8dcf83bb332fd898f33
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-03-22 14:07:58 -0700

    -Stuff

--- src/main/java/corgitaco/betterweather/server/BetterWeatherGameRules.java
+++ src/main/java/corgitaco/betterweather/server/BetterWeatherGameRules.java
@@ -8,7 +8,7 @@ public class BetterWeatherGameRules {
     public static final GameRules.RuleKey<GameRules.BooleanValue> DO_SEASON_CYCLE = register("doSeasonCycle", GameRules.Category.UPDATES, GameRules.BooleanValue.create(true));
 
     public static <T extends GameRules.RuleValue<T>> GameRules.RuleKey<T> register(String name, GameRules.Category category, GameRules.RuleType<T> type) {
-        if (ModList.get().isLoaded("sereneseasons") && name.equals("doSeasonCycle"))
+        if (name.equals("doSeasonCycle"))
             name = "doBWSeasonCycle";
 
         GameRules.RuleKey<T> rulekey = new GameRules.RuleKey<>(name, category);
