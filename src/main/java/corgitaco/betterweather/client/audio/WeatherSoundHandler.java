ab01d9c38391bae59bac15bad1779bb3ecfbe3eb
Tom Martin <tom.martin1239@gmail.com>
2021-05-30 05:00:05 +0100

    Betetr Sound (#56)

--- src/main/java/corgitaco/betterweather/client/audio/WeatherSoundHandler.java
+++ src/main/java/corgitaco/betterweather/client/audio/WeatherSoundHandler.java
@@ -13,11 +13,15 @@ import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.*;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherSoundHandler implements IAmbientSoundHandler {
 
@@ -91,25 +95,64 @@ public class WeatherSoundHandler implements IAmbientSoundHandler {
             this.maxVolume = volume;
         }
 
+        private static Vector3d[] vectorDistribution(int samples) {
+            List<Vector3d> points = new ArrayList<>();
+            double phi = Math.PI * (3. - Math.sqrt(5.));  // golden angle in radians
+
+            for (int i = samples/4; i < samples; i++) {
+                double y = 1 - (i / (float)(samples - 1)) * 2; //y goes from 1 to -1
+                double radius = Math.sqrt(1 - y * y); //radius at y
+
+                double theta = phi * i; //golden angle increment
+
+                double x = Math.cos(theta) * radius;
+                double z = Math.sin(theta) * radius;
+
+                points.add(new Vector3d(x, -y, z));
+            }
+            return points.toArray(new Vector3d[0]);
+        }
+
         public void tick() {
             if (volume == 0.0) {
                 this.finishPlaying();
             }
 
-            if (overrideRainStrength) {
-                if (this.fadeInTicks < 0) {
-                    this.finishPlaying();
+            Vector3d startPos = Minecraft.getInstance().gameRenderer.getActiveRenderInfo().getProjectedView();
+            int vectorDistance = 10;
+            double maxDistanceNormalised = 0;
+
+            for (Vector3d vec : vectorDistribution(10000)) {
+                Vector3d endPos = startPos.add(vec.scale(vectorDistance));
+                BlockRayTraceResult result = world.rayTraceBlocks(new RayTraceContext(startPos, endPos, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, null));
+
+                if(result.getType() == RayTraceResult.Type.MISS) {
+                    maxDistanceNormalised += maxVolume;
                 }
 
-                this.fadeInTicks += this.fadeSpeed;
-                this.volume = MathHelper.clamp((float) this.fadeInTicks / 40.0F, 0.0F, maxVolume);
-
-                if (this.volume == maxVolume) {
-                    overrideRainStrength = false;
-                }
-            } else {
-                this.volume = MathHelper.clamp(world.getRainStrength(Minecraft.getInstance().getRenderPartialTicks()), 0.0F, maxVolume);
+                Vector3d hitVec = result.getHitVec();
+                Vector3d dif = startPos.subtract(hitVec);
+                float distance = (float) dif.length() / vectorDistance;
+//                if(distance > maxDistanceNormalised) {
+                    maxDistanceNormalised += distance;
+//                }
             }
+            volume = (float)maxDistanceNormalised/10000;
+
+//            if (overrideRainStrength) {
+//                if (this.fadeInTicks < 0) {
+//                    this.finishPlaying();
+//                }
+//
+//                this.fadeInTicks += this.fadeSpeed;
+//                this.volume = MathHelper.clamp((float) this.fadeInTicks / 40.0F, 0.0F, maxVolume);
+//
+//                if (this.volume == maxVolume) {
+//                    overrideRainStrength = false;
+//                }
+//            } else {
+//                this.volume = MathHelper.clamp(world.getRainStrength(Minecraft.getInstance().getRenderPartialTicks()), 0.0F, maxVolume);
+//            }
         }
 
         public void fadeOutSound() {
